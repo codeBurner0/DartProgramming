3e596f23c7e6a367c2136ebc9734185e6b40bfa9
Ankit <codeburner0@gmail.com>
2022-06-10 15:26:11 +0530

    new change

--- Assignments/Assignment5/MathematicalCalculation/Using lambda Expression/class.dart
+++ Assignments/Assignment5/MathematicalCalculation/Using lambda Expression/class.dart
@@ -2,21 +2,22 @@ class Math {
   int? num1;
   int? num2;
   Math(this.num1, this.num2);
-// function for addition
+// lambda expression for addition
   int Addition(int num1, int num2) => num1 + num2;
 
-// function for substraction
+// lambda expression for substraction
   int Subtraction(int num1, int num2) => num1 - num2;
 
-// function for multiplication
+// lambda expression for multiplication
   int Multiplication(int num1, int num2) => num1 * num2;
 
-// function for integer division
+// lambda expression for integer division
   int IntegerDivision(int num1, int num2) => num1 ~/ num2;
 
+// lambda expression for Division
   double Division(int num1, int num2) => num1 / num2;
 
-// function for comparision
+// lambda expression for comparision
   String Comparision(int num1, int num2) => ((num1 > num2) && (num2 > num1))
       ? "Both are unequal."
       : "Both are equal.";
